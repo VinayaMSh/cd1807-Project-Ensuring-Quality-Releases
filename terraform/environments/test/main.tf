a595ac6596ee41280a5092e28b48987dd8332e56
VinayaMSh <37274214+VinayaMSh@users.noreply.github.com>
2024-04-11 18:09:36 +0200

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -20,7 +20,7 @@ provider "azurerm" {
 
 module "resource_group" {
   source               = "../../modules/resource_group"
-  resource_group       = "${var.resource_group}"
+  resource_group       = "${var.resource_group_name}"
   location             = "${var.location}"
 }
 
