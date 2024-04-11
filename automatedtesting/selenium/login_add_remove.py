99d31a9df7efcc6a7c4a54d56af639fd8c5c9ea3
VinayaMSh <37274214+VinayaMSh@users.noreply.github.com>
2024-04-11 23:42:29 +0200

    Create login_add_remove.py

---
+++ automatedtesting/selenium/login_add_remove.py
@@ -0,0 +1,40 @@
+# #!/usr/bin/env python
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+# Start the browser and login with standard_user
+def login (user, password):
+    print ('Starting the browser...')
+    # --uncomment when running in Azure DevOps.
+    # options = ChromeOptions()
+    # options.add_argument("--headless") 
+    # driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome()
+    print ('Browser started successfully. Navigating to the demo page to login.')
+    driver.get('https://www.saucedemo.com/')
+    
+    driver.find_element(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
+    driver.find_element(By.CSS_SELECTOR, "input[id='password']").send_keys(password)
+    
+    print ('Loggig in ..')
+    driver.find_element(By.CSS_SELECTOR, "input[id='login-button']").click()
+    time.sleep(1) 
+    
+    url_after_login = "https://www.saucedemo.com/inventory.html" 
+    print(driver.current_url)
+    
+    assert driver.current_url in url_after_login
+    
+    print ('Adding Sauce Labs Backpack to cart')
+    driver.find_element(By.CSS_SELECTOR, "button[id='add-to-cart-sauce-labs-backpack']").click()
+    time.sleep(1)
+    
+    print ('Removing item from cart')
+    driver.find_element(By.CSS_SELECTOR, "button[id='remove-sauce-labs-backpack']").click()
+    time.sleep(1)
+    
+    
+login('standard_user', 'secret_sauce')
+
