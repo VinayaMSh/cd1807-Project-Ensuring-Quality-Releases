e890413eb7fdbd037b765c9e0b0ff980026bc556
VinayaMSh <37274214+VinayaMSh@users.noreply.github.com>
2024-04-11 23:25:31 +0200

    Update login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -1,6 +1,7 @@
 # #!/usr/bin/env python
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.common.by import By
+import time
 
 
 # Start the browser and login with standard_user
@@ -13,6 +14,18 @@ def login (user, password):
     driver = webdriver.Chrome()
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
-
+    
+    driver.find_element(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
+    driver.find_element(By.CSS_SELECTOR, "input[id='password']").send_keys(password)
+    
+    driver.find_element(By.CSS_SELECTOR, "input[id='login-button']").click()
+    time.sleep(3) 
+    
+    url_after_login = "https://www.saucedemo.com/inventory.html" 
+    print(driver.current_url)
+    
+    assert driver.current_url in url_after_login
+    
+    
 login('standard_user', 'secret_sauce')
 
