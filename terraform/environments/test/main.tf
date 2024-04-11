b7c6009681eb8ce2d762f1eae04037a30a3fbcad
VinayaMSh <37274214+VinayaMSh@users.noreply.github.com>
2024-04-11 14:49:20 +0200

    Update main.tf
    added module for VM

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -52,3 +52,11 @@ module "publicip" {
   resource_type    = "publicip"
   resource_group   = "${module.resource_group.resource_group_name}"
 }
+module "vm" {
+  source           = "../../modules/vm"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  subnet_id        = "${module.network.subnet_id_test}"
+  resource_type    = "VM"
+  resource_group   = "${module.resource_group.resource_group_name}"
+}
