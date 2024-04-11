c9f14d71c3ac5464b6ca6310ab4d0d430615d49a
VinayaMSh <37274214+VinayaMSh@users.noreply.github.com>
2024-04-11 15:59:54 +0200

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -20,7 +20,7 @@ resource "azurerm_linux_virtual_machine" "test" {
   network_interface_ids = [azurerm_network_interface.test.id,]
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDdpe+2BeFyEG4HLde2VtLnf/xWpfb3Xk0EGGNNK8BVhF3hUzsON+ILfHwlYdsnMIJl+xCVW9YTWYbNIriGyKIBHbTNXh3KZ70iHLDRjgkwsnzRMeSTkARSxgV/ctSpg48DJvxMhyfmVwWLDw2LMeuXiC1cJ1OvrCXSGjM+APJt4dRBMBaMWXc4IdkgJBZBbwuDbRdPcOC/6RpujqB2NgxMDsqj0giI2qnewLr/GF98Ru4QboS/SrnPuXlpQBumP0A65GGTM4dajZvjkt2uVA7DHwSFMScxTq2f3oh/wNB8uSb4cs4StPWiWwcNktXIfWEMm9iQrDomdawW0ojqdY2GVpW+E3tOK1qni1LoBgJNOEjoUg1grN88uDMryNsmfYUpzymBywGh0W54d2EycxrfLpIDsKlvkEn1AWMRLEmAw7jgQhwLznflGMuGt+k9y5ZWVpjoXVSb5REhRwF/EhM/VkfFaB+A+Fr7GUKSdt5TYPSjy0YbBixi4Pob3TUrv0s= vinaya@SandboxHost-638453415541757999"
   }
   os_disk {
     caching           = "ReadWrite"
