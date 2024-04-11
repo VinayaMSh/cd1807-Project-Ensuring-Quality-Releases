4f0c51a6e8950d8e43b6170a12d1c40c308512ac
VinayaMSh <37274214+VinayaMSh@users.noreply.github.com>
2024-04-11 18:35:05 +0200

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -33,4 +33,25 @@ module "resource_group" {
   location             = "${var.location}"
 }
 
+module "network" {
+  source               = "../../modules/network"
+  address_space        = "${var.address_space}"
+  location             = "${var.location}"
+  virtual_network_name = "${var.virtual_network_name}"
+  application_type     = "${var.application_type}"
+  resource_type        = "NET"
+  resource_group       = "${module.resource_group.resource_group_name}"
+  address_prefix_test  = "${var.address_prefix_test}"
+}
+
+module "nsg-test" {
+  source           = "../../modules/networksecuritygroup"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "NSG"
+  resource_group   = "${module.resource_group.resource_group_name}"
+  subnet_id        = "${module.network.subnet_id_test}"
+  address_prefix_test = "${var.address_prefix_test}"
+}
+
 
