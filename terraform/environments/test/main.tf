31894795d1d0837173225509fe4479a8ebcd371f
VinayaMSh <37274214+VinayaMSh@users.noreply.github.com>
2024-04-11 18:08:11 +0200

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -11,12 +11,17 @@ terraform {
 }
 
 provider "azurerm" {
+  tenant_id       = "${var.tenant_id}"
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
-  location = "westus2"
+module "resource_group" {
+  source               = "../../modules/resource_group"
+  resource_group       = "${var.resource_group}"
+  location             = "${var.location}"
 }
 
 
