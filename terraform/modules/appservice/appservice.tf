97da46b4f54025b611f65f0ae31f51cc1dff35ed
VinayaMSh <37274214+VinayaMSh@users.noreply.github.com>
2024-04-12 19:35:29 +0200

    Update appservice.tf

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -17,5 +17,9 @@ resource "azurerm_linux_web_app" "test" {
   }
   site_config {
     always_on = false
+    application_stack {
+      current_stack   = "dotnet"
+      dotnet_version  = "v4.0"
+    } 
   }
 }
