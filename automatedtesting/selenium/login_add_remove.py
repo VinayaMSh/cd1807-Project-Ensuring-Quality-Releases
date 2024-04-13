4fd26247b01e847427f1cccb3b094ec316fce2a5
VinayaMSh <37274214+VinayaMSh@users.noreply.github.com>
2024-04-13 02:34:11 +0200

    Update login_add_remove.py

--- automatedtesting/selenium/login_add_remove.py
+++ automatedtesting/selenium/login_add_remove.py
@@ -1,6 +1,7 @@
 # #!/usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 import time
 
 
@@ -8,10 +9,10 @@ import time
 def login (user, password):
     print ('Starting the browser...')
     # --uncomment when running in Azure DevOps.
-    # options = ChromeOptions()
-    # options.add_argument("--headless") 
-    # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
+    options = ChromeOptions()
+    options.add_argument("--headless") 
+    driver = webdriver.Chrome(options=options)
+    #driver = webdriver.Chrome()
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
     
